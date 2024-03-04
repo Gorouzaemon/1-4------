c8ee2d17c8873b9ac7ff3aa63a50ad06e5aa621c
gorouzaemon <stngdss051@outlook.jp>
2024-03-04 13:15:09 +0900

    90%完了

--- js/transition.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load',function(){
-  $('#splash-logo').delay(1200).fadeOut('slow');
-  $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-    
-    $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-})
-
-});
